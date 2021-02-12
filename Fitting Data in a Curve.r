266c002793d706f71b0135e251d3084cfbdc3904
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-02-12 22:05:12 +0600

    Produced distribution vector from frequency table.

--- Fitting Data in a Curve.r
+++ Fitting Data in a Curve.r
@@ -1,13 +1,22 @@
-data = read.csv(file = "tdata.csv", header=TRUE)
-distwp = data[1]
-distwp = distwp[!is.na(distwp)]
-distsp = data[2]
-distsp = distsp[!is.na(distsp)]
-distc = data[3]
-distc = distc[!is.na(distc)]
-distnc = data[4]
-distnc = distnc[!is.na(distnc)]
+data = read.csv(file = "data.csv", header=TRUE)
 
+freq_tab_standing_pedestrian = data.frame(value=data[1][!is.na(data[1])],freq=data[2][!is.na(data[2])])
+
+freq_tab_standing_pedestrian
+
+dist_standing_pedestrian = rep(freq_tab_standing_pedestrian$value,freq_tab_standing_pedestrian$freq)
+
+
+# Old Code
+# distwp = data[1]
+# distwp = distwp[!is.na(distwp)]
+# distsp = data[2]
+# distsp = distsp[!is.na(distsp)]
+# distc = data[3]
+# distc = distc[!is.na(distc)]
+# distnc = data[4]
+# distnc = distnc[!is.na(distnc)]
+# 
 # hist(distwp,breaks=ceiling(max(distwp)),
 #      col=rgb(0,0,1,1/4),xlab="Distance of Walking Pedastrian", 
 #      cex.lab=1.3,cex.axis=1.3, col.axis="blue")
@@ -23,18 +32,14 @@ distnc = distnc[!is.na(distnc)]
 #      cex.lab=1.3,cex.axis=1.3, col.axis="blue")
 # curve(length(distc)*dnorm(x,mean=mean(distc), sd = sd(distc)), 
 #       col="darkblue", lwd=2, add=TRUE, yaxt="n")
-
-density_wp = density(distwp)
-density_sp = density(distsp)
-density_c = density(distc)
-density_nc = density(distnc)
-plot(density_wp)
-plot(density_sp)
-plot(density_c)
-plot(density_nc)
-curve(0.475*dnorm(x,mean=1,sd=0.5)+0.18*dnorm(x,mean=2,sd=0.4)+0.27*dnorm(x,mean=3,sd=0.6),
-      col="darkblue", lwd=2, add=TRUE, yaxt="n")
-
-
-
-
+# 
+# density_wp = density(distwp)
+# density_sp = density(distsp)
+# density_c = density(distc)
+# density_nc = density(distnc)
+# plot(density_wp)
+# plot(density_sp)
+# plot(density_c)
+# plot(density_nc)
+# curve(0.475*dnorm(x,mean=1,sd=0.5)+0.18*dnorm(x,mean=2,sd=0.4)+0.27*dnorm(x,mean=3,sd=0.6),
+#       col="darkblue", lwd=2, add=TRUE, yaxt="n")
