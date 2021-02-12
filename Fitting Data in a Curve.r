306d65a5cda8834e7d5861bdaaf7aed8465e706e
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-02-13 03:39:18 +0600

    Constructed the distribution for parked cars.

--- Fitting Data in a Curve.r
+++ Fitting Data in a Curve.r
@@ -2,14 +2,16 @@ data = read.csv(file = "data.csv", header=TRUE)
 
 freq_tab_stndng_ped = data.frame(value=data[1][!is.na(data[1])], freq=data[2][!is.na(data[2])])
 freq_tab_parked_car = data.frame(value=data[3][!is.na(data[3])], freq=data[4][!is.na(data[4])])
+freq_tab_parked_rck = data.frame(value=data[5][!is.na(data[5])], freq=data[6][!is.na(data[6])])
+freq_tab_parked_cng = data.frame(value=data[7][!is.na(data[7])], freq=data[8][!is.na(data[8])])
 
 dist_stndng_ped = rep(freq_tab_stndng_ped$value, freq_tab_stndng_ped$freq)
 dist_parked_car = rep(freq_tab_parked_car$value, freq_tab_parked_car$freq)
+dist_parked_rck = rep(freq_tab_parked_rck$value, freq_tab_parked_rck$freq)
+dist_parked_cng = rep(freq_tab_parked_cng$value, freq_tab_parked_cng$freq)
 
 plot(density(dist_stndng_ped))
-plot(density(dist_parked_car))
-
-plot(density(dist_stndng_ped))
+# Distribution of standing pedestrians
 curve(
   0.472*dnorm(x,mean=0.825,sd=0.4)
   +0.33*dnorm(x,mean=1.925,sd=0.45)
@@ -17,3 +19,19 @@ curve(
   +0.0485*dnorm(x,mean=3.73,sd=0.32)
   +0.007*dnorm(x,mean=5.75,sd=0.4),
   col="red", lwd=2, add=TRUE)
+
+
+plot(density(dist_parked_car))
+# Distribution of parked cars
+curve(
+  0.06*dnorm(x,mean=1.1,sd=0.3575)
+  +0.542*dnorm(x,mean=2.07,sd=0.322)
+  +0.17*dnorm(x,mean=3.64,sd=0.332)
+  +0.18*dnorm(x,mean=2.76,sd=0.29)
+  +0.038*dnorm(x,mean=4.48,sd=0.27)
+  +0.01*dnorm(x,mean=5.92,sd=0.3),
+  col="red", lwd=2, add=TRUE)
+
+plot(density(dist_parked_rck))
+
+plot(density(dist_parked_cng))
