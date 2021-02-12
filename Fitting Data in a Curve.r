ef21d53ab6c1848d57c4c5f87d0b4c71c31b15a6
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-02-12 21:49:44 +0600

    Initial commit.

---
+++ Fitting Data in a Curve.r
@@ -0,0 +1,40 @@
+data = read.csv(file = "tdata.csv", header=TRUE)
+distwp = data[1]
+distwp = distwp[!is.na(distwp)]
+distsp = data[2]
+distsp = distsp[!is.na(distsp)]
+distc = data[3]
+distc = distc[!is.na(distc)]
+distnc = data[4]
+distnc = distnc[!is.na(distnc)]
+
+# hist(distwp,breaks=ceiling(max(distwp)),
+#      col=rgb(0,0,1,1/4),xlab="Distance of Walking Pedastrian", 
+#      cex.lab=1.3,cex.axis=1.3, col.axis="blue")
+# 
+# hist(distsp,breaks=ceiling(max(distsp)),
+#      col=rgb(0,0,1,1/4),xlab="Distance of Standing Pedastrian", 
+#      cex.lab=1.3,cex.axis=1.3, col.axis="blue")
+# curve(10*dbeta(x/6,1.5,2.5), 
+#       col="darkblue", lwd=2, add=TRUE, yaxt="n")
+# 
+# hist(distc,breaks=ceiling(max(distc)),
+#      col=rgb(0,0,1,1/4),xlab="Distance of Car", 
+#      cex.lab=1.3,cex.axis=1.3, col.axis="blue")
+# curve(length(distc)*dnorm(x,mean=mean(distc), sd = sd(distc)), 
+#       col="darkblue", lwd=2, add=TRUE, yaxt="n")
+
+density_wp = density(distwp)
+density_sp = density(distsp)
+density_c = density(distc)
+density_nc = density(distnc)
+plot(density_wp)
+plot(density_sp)
+plot(density_c)
+plot(density_nc)
+curve(0.475*dnorm(x,mean=1,sd=0.5)+0.18*dnorm(x,mean=2,sd=0.4)+0.27*dnorm(x,mean=3,sd=0.6),
+      col="darkblue", lwd=2, add=TRUE, yaxt="n")
+
+
+
+
