1da810268b4f16c3f230a91e13fe86aa426005ac
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-02-14 19:41:45 +0600

    Had a better insight.

--- Fitting Data in a Curve.r
+++ Fitting Data in a Curve.r
@@ -13,40 +13,54 @@ dist_parked_cng = rep(freq_tab_parked_cng$value, freq_tab_parked_cng$freq)
 plot(density(dist_stndng_ped))
 # Distribution of standing pedestrians
 curve(
-  0.472*dnorm(x,mean=0.825,sd=0.4)
-  +0.33*dnorm(x,mean=1.925,sd=0.45)
-  +0.14*dnorm(x,mean=2.97,sd=0.4)
-  +0.0485*dnorm(x,mean=3.73,sd=0.32)
-  +0.007*dnorm(x,mean=5.75,sd=0.4),
+  +0.4720*dnorm(x,mean=0.825,sd=0.40)
+  +0.3300*dnorm(x,mean=1.925,sd=0.45)
+  +0.1400*dnorm(x,mean=2.970,sd=0.40)
+  +0.0485*dnorm(x,mean=3.730,sd=0.32)
+  +0.0070*dnorm(x,mean=5.750,sd=0.40),
   col="red", lwd=2, add=TRUE)
 
 
 plot(density(dist_parked_car))
 # Distribution of parked cars
 curve(
-  0.06*dnorm(x,mean=1.1,sd=0.3575)
-  +0.542*dnorm(x,mean=2.07,sd=0.322)
-  +0.17*dnorm(x,mean=3.64,sd=0.332)
-  +0.18*dnorm(x,mean=2.76,sd=0.29)
-  +0.038*dnorm(x,mean=4.48,sd=0.27)
-  +0.01*dnorm(x,mean=5.92,sd=0.3),
+  +0.060*dnorm(x,mean=1.10,sd=0.3575)
+  +0.542*dnorm(x,mean=2.07,sd=0.3220)
+  +0.170*dnorm(x,mean=3.64,sd=0.3320)
+  +0.180*dnorm(x,mean=2.76,sd=0.2900)
+  +0.038*dnorm(x,mean=4.48,sd=0.2700)
+  +0.010*dnorm(x,mean=5.92,sd=0.3000),
   col="red", lwd=2, add=TRUE)
 
 
 plot(density(dist_parked_rck))
 # Distribution of parked rickshaws
 curve(
-  0.6*dnorm(x,mean=1.55,sd=0.52)
+  +0.600*dnorm(x,mean=1.55,sd=0.520)
   +0.232*dnorm(x,mean=2.75,sd=0.415)
-  +0.09*dnorm(x,mean=3.68,sd=0.3)
-  +0.046*dnorm(x,mean=4.65,sd=0.38)
-  +0.009*dnorm(x,mean=5.8,sd=0.38),
+  +0.090*dnorm(x,mean=3.68,sd=0.300)
+  +0.046*dnorm(x,mean=4.65,sd=0.380)
+  +0.009*dnorm(x,mean=5.80,sd=0.380),
   col="red", lwd=2, add=TRUE)
 
 
 plot(density(dist_parked_cng))
 # Distribution of parked CNGs
 curve(
-  0.9*dnorm(x,mean=2.2,sd=0.7)
-  +0.035*dnorm(x,mean=3.85,sd=0.2),
+  +0.900*dnorm(x,mean=2.20,sd=0.70)
+  +0.035*dnorm(x,mean=3.85,sd=0.20),
   col="red", lwd=2, add=TRUE)
+
+# Test Stuffs
+x = seq(-10, 10, by = .01)
+y = dnorm(x,mean=3,sd=.8)
+plot(x,y)
+
+n = 1000
+# aList = rnorm(n/0.3,mean=1,sd=0.3)+rnorm(n/0.7,mean=4,sd=0.5)
+aList = rnorm(n,mean=2,sd=0.8)
+# aList = rnorm(n/0.7,mean=4,sd=0.5)
+aList = sort(aList)
+plot(aList)
+hist(aList)
+
